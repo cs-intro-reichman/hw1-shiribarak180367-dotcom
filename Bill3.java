4260787dd2ebf1fe6eef0b8d991a288ee1bc8246
shiribarak180367-dotcom <shiribarak180367@gmail.com>
2025-10-29 18:30:26 +0200

    finished Bill3 assignment

--- Bill3.java
+++ Bill3.java
@@ -3,6 +3,11 @@ public class Bill3 {
 	public static void main(String[] args) {
 		// To get you started, here is the first line in the program:
 	    String name1 = args[0];
-	    // Replace this comment with the rest of your code   
+	    String name2 = args[1];
+		String name3 = args[2];
+		int totalBill = Integer.parseInt(args[3]);
+		double perPerson = Math.ceil (totalBill / 3.0);
+		String message = "dear " + name3 + " " + name2 + " , and " + name1 + ": pay" + perPerson + " Shekels each.";
+		System.out.println(message);
 	}
 }
