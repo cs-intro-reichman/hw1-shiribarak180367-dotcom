c0a5302945c5677ae0bf40ef338572869738054c
Shiri Barak <shiri.barak@post.runi.ac.il>
2025-11-02 11:13:28 +0200

    fixed NumWords

--- FVCalc.java
+++ FVCalc.java
@@ -5,10 +5,10 @@ public class FVCalc {
 	int currentValue = Integer.parseInt(args[0]);
 	double rate = Double.parseDouble(args[1]);
 	int years = Integer.parseInt(args[2]);
-	rate = rate / 100;
-	double factor = Math.pow(1 + rate, years);
-	double futureValue = (currentValue * factor);
-	System.out.println("after " + years + " years, " + "saved at " + rate + "% will yield $" + (int)futureValue);
+	rate /= 100.0;
+	double factor = Math.pow(1.0 + rate, (double)years);
+	double futureValue = (double)currentValue * factor;
+	System.out.println("after " + years + " years, saved at " + rate + "% will yield $" + (int)futureValue);
 	}
 }
 	
--- NumWords.java
+++ NumWords.java
@@ -2,10 +2,10 @@
 public class NumWords {
 	public static void main(String args[]) {
    int number = Integer.parseInt(args[0]);
-   int hundreds =  number/ 100;
-   int tens = (number % 100) / 10;
+   int hundreds =  number / 100;
+   int tens = number % 100 / 10;
    int ones = number % 10;
 
-   System.out.println( hundreds + " hundreds, " + tens + " tens and " + ones + " ones.");
+   System.out.println("" + hundreds + " hundreds, " + tens + " tens and " + ones + " ones.");
 	}
 }
